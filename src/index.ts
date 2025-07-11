800d69345629958ff6573fd37224a4c8fe68e34b
Mahiru Shiina <halovemusics@gmail.com>
2025-07-11 10:18:40 +0700

    Cấu hình kết nối database

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-// import * as database from "./config/database";
-// import mainV1Routes from "./api/v1/routes/index.route";
-import cors from "cors";
+ import * as database from "./config/database";
+
 import cookieParser from "cookie-parser";
 
-
-
-
-dotenv.config();
-//database.connect();
-
+// Khai báo app
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+// Cấu hình .env
+dotenv.config();
+//Kết nối Database
+database.connect();
+
 
 // Dùng khi cài bug
 app.set("views", "./views");
@@ -22,10 +22,10 @@ app.set("view engine", "pug");
 app.use(express.json()); // Đọc JSON từ client (axios/fetch gửi lên)
 app.use(express.urlencoded({ extended: true })); // Nếu dùng form HTML gửi lên
 
-app.use(cors());
+//  dùng khi muốn res.cookie
 app.use(cookieParser());
 
-//mainV1Routes(app);
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
